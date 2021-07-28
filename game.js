f3a5a9de4e75bb428e339746f938554b41c6bb9e
mdavies <ms.melissadavies@gmail.com>
2021-07-28 23:59:18 +0200

    Add other sprites

--- game.js
+++ game.js
@@ -3,14 +3,21 @@ kaboom({
 	fullscreen: true,
 	scale: 1,
 	debug: true,
-	// clearColor: [0, 0, 0, 1],
+	clearColor: [0, 0, 0, 1],
 });
 
 
-loadSprite('diamond', '/assets/diamond.png');
-loadSprite('lady', '/assets/lady.png');
+loadSprite("diamond", "assets/diamond.png");
+// loadSprite('lady', 'assets/lady.png');
+// loadSprite('lady', 'assets/cupcake.png');
+// loadSprite('lady', 'assets/cactus.png');
 
-const diamond = addSprite('diamond', {
-	pos: vec2(80, 80),
-	body: true,
+// const diamond = sprite("diamond", {
+// 	pos: vec2(80, 80),
+// 	body: true,
+// });
+
+addRect(width(), 20, {
+	pos: vec2(0, height() - 40),
+	solid: true,
 });
